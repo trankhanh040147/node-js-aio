bb2c29965de7f3e8fe6cb29368700aca06e3924c
trankhanh040147 <trankhanh040147@gmail.com>
2024-01-11 09:56:49 +0700

    add fullstackopen\simple-web-server

---
+++ node-course/fullstackopen/simple-web-server/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const app = http.createServer(
+    (request, response) => {
+        response.writeHead(200, {'Content-Type': 'text/plain'})
+        response.end('Hello World')
+    }
+)
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`);
