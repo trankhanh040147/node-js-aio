ae224c8b34cc944385123631372dadca8ec1ddde
trankhanh040147 <trankhanh040147@gmail.com>
2024-01-12 22:10:40 +0700

    Phonebook backend step3

--- node-course/fullstackopen/simple-web-server/index.js
+++ node-course/fullstackopen/simple-web-server/index.js
@@ -20,6 +20,17 @@ let notes = [
   }
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0 
+    ? Math.max(...notes.map(n=>n.id))
+    : 1
+  return maxId + 1
+}
+
+// setting up 
+app.use(express.json()) // parse incoming JSON data to `request.body` property
+
+
 // use `http` module
 // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { 'Content-Type': 'application/json' })
@@ -34,8 +45,55 @@ app.get('/api/note', (request, response) => {
   response.json(notes) // set response header to `application/json` and convert `notes` to JSON, then send it
 })
 
-const PORT = 3001
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id);
+  // const note = notes.find(note => {
+  //   console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+  //   return note.id === id
+  // })
+  const note = notes.find(note => note.id === id)
+  if(note) {
+    response.json(note)
+  } else {
+    response.status(404).send("The note with id " + id + " is not found.")
+  }
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if(!body.content){
+    return response.status(400).json({
+      error: "content missing"
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
